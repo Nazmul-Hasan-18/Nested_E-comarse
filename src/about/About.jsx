41aa792f29d20aee46d8bbc94112b1271495766e
Nazmul Hasan <nazmulhasan.official98@gmail.com>
2025-12-17 14:00:45 +0600

    change

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -4,7 +4,7 @@ import Section from './aboutComponent/section/Section';
 import Perfomance from './aboutComponent/perfomance/Perfomance';
 import Misson from './aboutComponent/misson/Misson';
 import Team from './aboutComponent/ourTeam/Team';
-import Subscribe from './aboutComponent/Subscribe/Subscribe';
+import Subscribe from './aboutComponent/subscribe/Subscribe';
 import BestPrice from '../home page/HomeComponent/bestprice/BestPrice';
 
 
